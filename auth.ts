d1c3b249773a5127b0e93350d391acad8772538c
TakayukiHirano117 <ikusa.hirano@icloud.com>
2024-07-08 16:29:17 +0900

    commit

--- auth.ts
+++ auth.ts
@@ -1,8 +1,14 @@
 import NextAuth, { NextAuthConfig } from "next-auth";
 import github from "next-auth/providers/github";
+import google from "next-auth/providers/google";
 
 export const config: NextAuthConfig = {
-  providers: [github],
+  providers: [
+    github({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+  ],
   basePath: "/api/auth",
   callbacks: {
     authorized({ request, auth }) {
